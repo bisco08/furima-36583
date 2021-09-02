a05bfbf635ff27c4f30e1e2071804f50941e0a92
bisco08 <88226995+bisco08@users.noreply.github.com>
2021-09-02 18:43:00 +0900

    修正済み

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :item_params, only: [:show, :edit]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @item = Item.order(created_at: :DESC)
@@ -19,14 +21,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    unless current_user.id == @item.user.id
-      redirect_to root_path
-    end
   end
 
   def update
@@ -42,5 +39,15 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:item_name, :content, :price, :category_id, :condition_id, :area_id, :shipping_charge_id, :shipping_date_id, :image).merge(user_id: current_user.id)
+  end
+
+  def item_params
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 end
